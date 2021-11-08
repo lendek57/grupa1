b0a00aec624eb26701b845ac93a44cf1394e8ecf
Lena Dekster <lenadekster@gmail.com>
2021-11-08 17:29:02 +0100

    Add periodic border conditions

---
+++ src/main/java/simulation/Simulation.java
@@ -0,0 +1,14 @@
+package simulation;
+
+public class Simulation {
+    private static final int WIDTH = 20, HEIGHT = 30;
+    private static final WorldMap map = new WorldMap(WIDTH, HEIGHT);
+
+    public static WorldMap getMap() {
+        return map;
+    }
+
+    public static void simulateDay() {
+        map.run();
+    }
+}
